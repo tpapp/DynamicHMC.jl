421c026229a2f2953ab5b53d73e046705d2631b9
Tamas K. Papp <tkpapp@gmail.com>
2017-06-14 20:59:08 +0200

    Initial commit.

--- src/DynamicHMC.jl
+++ src/DynamicHMC.jl
@@ -1,5 +1,52 @@
+"""
+Notation follows Betancourt (2017), with some differences
+
+## Density
+- `q` is the parameter vector
+- `π(q)` is the density we are sampling from
+- V(q) = -log(π(q)) ≡ -ℓ(q), in practice we use ℓ
+
+## Momentym
+- p is the momentum
+- K(p,q) = -log π(p|q) is minus log conditional density for the momentum
+- in practice we use ω for the momentum
+"""
 module DynamicHMC
 
-# package code goes here
+import Base: rand
+
+export
+    logdensity,
+    loggradient,
+    UnitNormal,
+    UNITNORMAL,
+    minuslogH,
+    leapfrog
+
+"""
+Unit normal momentum. Efficient when the distribution has been
+decorrelated.
+
+p|q ∼ MultivariateNormal(0,I) (independently of `q`)
+"""
+struct UnitNormal end
+
+UNITNORMAL = UnitNormal()
+
+logdensity(::UnitNormal, p, q) = -sum(abs2, p)/2
+
+rand(::UnitNormal, q) = randn(length(q))
+
+"Jacobian of the log density."
+function loggradient end
+
+function leapfrog(ℓ, ω::UnitNormal, q, p, ϵ)
+    p₊ = p + ϵ/2 * loggradient(ℓ, q)
+    q′ = q + ϵ * p₊
+    p′ = p₊ + ϵ/2 * loggradient(ℓ, q′)
+    q′, p′
+end
+
+minuslogH(ℓ, ω, q, p) = logdensity(ω, p, q) + logdensity(ℓ, q)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,48 @@
 using DynamicHMC
+
+import DynamicHMC: logdensity, loggradient
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+"Normal density, used for testing."
+struct NormalDensity{T,S}
+    "Mean."
+    μ::Vector{T}
+    "Square root of the *inverse* of the variance matrix Σ, ie L'L=Σ."
+    L::S
+end
+
+logdensity(ℓ::NormalDensity, p) = -sum(abs2, ℓ.L*(p-ℓ.μ))/2
+loggradient(ℓ::NormalDensity, p) = -ℓ.L'*ℓ.L*(p-ℓ.μ)
+Base.rand(ℓ::NormalDensity) = ℓ.L \ (randn(length(ℓ.μ)) + ℓ.μ)
+
+"""
+Test that the Hamiltonian is invariant using the leapfrog integrator,
+normal with identity matrix.
+"""
+@testset "leapfrog" begin
+    ω = UNITNORMAL
+    ℓ = NormalDensity(fill(0.0, 3), I)
+    q = rand(ℓ)
+    p = rand(ω, q)
+    minuslogH₀ = minuslogH(ℓ, ω, q, p)
+    for i in 1:50
+        q, p = leapfrog(ℓ, ω, q, p, 0.01)
+        @test isapprox(minuslogH₀, minuslogH(ℓ, ω, q, p); rtol = 1e-3)
+    end
+end
+
+"Test that the Hamiltonian is invariant using the leapfrog integrator,
+non-centered normal with correlated matrix."
+@testset "leapfrog" begin
+    ω = UNITNORMAL
+    ℓ = NormalDensity(fill(0.0,3), [1.0 0.1 0.2;
+                                   0 2.0 0.3
+                                   0 0 0.5])
+    q = rand(ℓ)
+    p = rand(ω, q)
+    minuslogH₀ = minuslogH(ℓ, ω, q, p)
+    for i in 1:500
+        q, p = leapfrog(ℓ, ω, q, p, 0.1)
+        @test isapprox(minuslogH₀, minuslogH(ℓ, ω, q, p); rtol = 1e-3)
+    end
+end
