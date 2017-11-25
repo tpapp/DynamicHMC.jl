9a89af5e16152e64a88b648c1bccc905138e4e81
Tamas K. Papp <tkpapp@gmail.com>
2017-11-25 19:54:13 +0100

    building on 0.6 as nightly is broken ATM.

--- docs/make.jl
+++ docs/make.jl
@@ -19,5 +19,4 @@ deploydocs(repo = "github.com/tpapp/DynamicHMC.jl.git",
            # target = "build",
            deps = nothing,
            make = nothing,
-           # julia = "0.6"
-           )
+           julia = "0.6")
