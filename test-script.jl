4b7cc864fd3f7faf7372f0bd9132519cf1b5ae7b
Tamas K. Papp <tkpapp@gmail.com>
2018-02-13 14:29:42 +0100

    Fixed dependencies.
    The relevant DiffResults version was tagged since the then, and
    MCMCDiagnostics is now registered.

--- test-script.jl
+++ test-script.jl
@@ -1,4 +1,1 @@
-Pkg.clone("https://github.com/tpapp/MCMCDiagnostics.jl.git")
-Pkg.add("DiffResults")         # remove this and the next line when released ...
-Pkg.checkout("DiffResults")    # ... DiffResults precompiles
 Pkg.clone(pwd()); Pkg.build("DynamicHMC"); Pkg.test("DynamicHMC"; coverage=true)
