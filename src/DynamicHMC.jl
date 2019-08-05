5ffbab422e9c24973474dac1f8b39242eb51194c
Tamas K. Papp <tkpapp@gmail.com>
2019-08-05 18:01:04 +0200

    package docs blurb

--- src/DynamicHMC.jl
+++ src/DynamicHMC.jl
@@ -1,3 +1,13 @@
+"""
+Implementation of the No U-Turn Sampler for MCMC.
+
+Please read the documentation. For the impatient: you probably want to
+
+1. define a log density problem (eg for Bayesian inference) using the `LogDensityProblems`
+package, then
+
+2. use it with [`mcmc_with_warmup`](@ref).
+"""
 module DynamicHMC
 
 export
