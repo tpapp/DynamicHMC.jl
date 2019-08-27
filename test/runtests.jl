180cdabaa48771d8277d1a7a3386ae901cb356e6
Tamas K. Papp <tkpapp@gmail.com>
2019-08-27 10:58:12 +0200

    organize sample correctness utilities into a separate file (#78)
    this should facilitate interactive testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,10 +9,19 @@ macro include_testset(filename)
     end
 end
 
+####
+#### unit tests
+####
+
 @include_testset("test-trees.jl")
 @include_testset("test-hamiltonian.jl")
 @include_testset("test-NUTS.jl")
 @include_testset("test-stepsize.jl")
 @include_testset("test-mcmc.jl")
 @include_testset("test-diagnostics.jl")
-@include_testset("sample-correctness.jl")
+
+####
+#### sample correctness tests
+####
+
+@include_testset("sample-correctness-tests.jl")
