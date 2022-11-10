975ea8d179e45f4930d3dc464b3d5d7fdf5a394a
Tamás K. Papp <tkpapp@gmail.com>
2022-11-10 14:26:12 +0100

    try using module name directly

--- test/jet.jl
+++ test/jet.jl
@@ -2,10 +2,10 @@ if VERSION >= v"1.7"
 
     using JET
 
-    insert!(LOAD_PATH, 2, "..")
+    # insert!(LOAD_PATH, 2, "..")
 
     @testset "static analysis with JET.jl" begin
-        @test isempty(JET.get_reports(report_package("DynamicHMC", target_modules=(DynamicHMC,))))
+        @test isempty(JET.get_reports(report_package(DynamicHMC, target_modules=(DynamicHMC,))))
     end
 
 end
