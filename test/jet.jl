76625b5d34865a0668b44c9ebba72a309525b9e5
Tamás K. Papp <tkpapp@gmail.com>
2022-11-10 14:43:56 +0100

    insert load path again

--- test/jet.jl
+++ test/jet.jl
@@ -1,11 +1,7 @@
 if VERSION >= v"1.7"
-
     using JET
-
-    # insert!(LOAD_PATH, 2, "..")
-
+    insert!(LOAD_PATH, 2, "..")
     @testset "static analysis with JET.jl" begin
         @test isempty(JET.get_reports(report_package(DynamicHMC, target_modules=(DynamicHMC,))))
     end
-
 end
