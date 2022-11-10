2af9b2e953139eb35f9b644e9805c80e55514151
Tamás K. Papp <tkpapp@gmail.com>
2022-11-10 14:16:33 +0100

    environment workaround

--- test/jet.jl
+++ test/jet.jl
@@ -2,6 +2,8 @@ if VERSION >= v"1.7"
 
     using JET
 
+    insert!(LOAD_PATH, 2, "..")
+
     @testset "static analysis with JET.jl" begin
         @test isempty(JET.get_reports(report_package("DynamicHMC", target_modules=(DynamicHMC,))))
     end
