3ad4207523a6380618ff5188eb923c3b6a894c27
Tamás K. Papp <tkpapp@gmail.com>
2022-11-17 15:26:16 +0100

    fix

---
+++ src/utilities.jl
@@ -0,0 +1,38 @@
+#####
+##### utilities
+#####
+
+####
+#### error messages
+####
+
+"""
+$(TYPEDEF)
+
+The error type used by this package. Debug information should be printed without truncation,
+with full precision.
+
+$(FIELDS)
+"""
+struct DynamicHMCError <: Exception
+    message::String
+    debug_information::NamedTuple
+end
+
+"""
+$(SIGNATURES)
+
+Throw a `DynamicHMCError` with given message, keyword arguments used for debug information.
+"""
+_error(message::AbstractString; kwargs...) = throw(DynamicHMCError(message, NamedTuple(kwargs)))
+
+function Base.showerror(io::IO, error::DynamicHMCError)
+    @unpack message, debug_information = error
+    printstyled(io, "DynamicHMC error: ", error; color = :red)
+    for (key, value) in pairs(debug_information)
+        print(io, "\n  ")
+        printstyled(io, string(key); color = :blue, bold = true)
+        printstyled(io, " = ", value)
+    end
+    nothing
+end
