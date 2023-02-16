66845ff38c2f764a1a013cbaf6e304e6e61eb2d1
Tamás K. Papp <tkpapp@gmail.com>
2023-02-16 08:58:43 +0100

    remove workaround
    cf https://github.com/aviatesk/JET.jl/issues/412#event-8533525974

--- test/jet.jl
+++ test/jet.jl
@@ -1,5 +1,4 @@
 using JET
-insert!(LOAD_PATH, 2, "..")
 @testset "static analysis with JET.jl" begin
     @test isempty(JET.get_reports(report_package(DynamicHMC, target_modules=(DynamicHMC,))))
 end
